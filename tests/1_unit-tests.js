542398147a370d1ef1ab60be2df917b35dfd151e
steven-j-alexander <steven.james.alexander@gmail.com>
2017-09-03 16:27:59 -0500

    1 - Learn How Javascript Assertions Work

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,25 +2,25 @@ var chai = require('chai');
 var assert = chai.assert;
 
 suite('Unit Tests', function(){
-  
+
   // Make ALL tests pass
   // !! Don't scramble the Assertions. We rely on their order to check the results !!
   suite('Basic Assertions', function() {
     /** assert.fail() will always fail. Change it into something more useful... **/
-  
+
     /** 1 - Use assert.isNull() or assert.isNotNull() to make the tests pass. **/
     test('#isNull, #isNotNull', function(){
-      assert.fail(null, 'this is an optional error description - e.g. null is null');
-      assert.fail( 1, '1 is not null');
+      assert.isNull(null, 'this is an optional error description - e.g. null is null');
+      assert.isNotNull( 1, '1 is not null');
     });
-  
+
     /** 2 - Use assert.isDefined() or assert.isUndefined() to make the tests pass. **/
     test('#isDefined, #isUndefined', function(){
       assert.fail( null, 'null is not undefined');
       assert.fail( undefined, 'undefined IS undefined');
       assert.fail( 'hello', 'a string is not undefined' );
     });
-  
+
     /** 3 - Use assert.isOk() or assert.isNotOk() to make the tests pass. **/
     // .isOk(truthy) and .isNotOk(falsey) will pass
     test('#isOk, #isNotOk', function(){
@@ -28,7 +28,7 @@ suite('Unit Tests', function(){
       assert.fail( "I'm truthy", 'a string is truthy');
       assert.fail( true, 'true is truthy' );
     });
-  
+
     /** 4 - Use assert.isTrue() or assert.isNotTrue() to make the tests pass. **/
     // .isTrue(true) and .isNotTrue(everything else) will pass.
     // .isFalse() and .isNotFalse() also exist.
@@ -37,14 +37,14 @@ suite('Unit Tests', function(){
       assert.fail( !!'double negation', 'double negation of a truthy is true');
       assert.fail({ value: 'truthy' }, 'A truthy object is NOT TRUE (neither is false...)' );
     });
-  
-    // There are more assertions like these: .isNaN(), .isBoolean(), 
+
+    // There are more assertions like these: .isNaN(), .isBoolean(),
     // and many others. Almost all the assertions in the chai library
     // have their negative counterpart - e.g. .isNotBoolean(), ...
   });
-  
+
   // -----------------------------------------------------------------------------
-  
+
   suite('Equality', function() {
     /** 5 - .equal(), .notEqual() **/
     // .equal() compares objects using '=='
@@ -71,7 +71,7 @@ suite('Unit Tests', function(){
   });
 
 // -----------------------------------------------------------------------------
-  
+
 
   // This function is used in the tests. Don't Edit it.
   function weirdNumbers(delta) {
@@ -79,7 +79,7 @@ suite('Unit Tests', function(){
   }
 
   suite('Comparisons', function() {
-    
+
     /** 8 - .isAbove() => a > b , .isAtMost() => a <= b **/
     test('#isAbove, #isAtMost', function() {
       assert.fail('hello'.length , 5);
@@ -113,13 +113,13 @@ suite('Unit Tests', function(){
   var winterMonths = ['dec,','jan', 'feb', 'mar'];
   var backendLanguages = ['php', 'python', 'javascript', 'ruby', 'asp'];
   suite('Arrays', function(){
-    
+
     /** 11 - #isArray vs #isNotArray **/
     test('#isArray, #isNotArray', function() {
       assert.fail('isThisAnArray?'.split(''), 'String.prototype.split() returns an Array');
       assert.fail([1,2,3].indexOf(2), 'indexOf returns a number.');
     });
-    
+
     /** 12 - #include vs #notInclude **/
     test('Array #include, #notInclude', function() {
       assert.fail(winterMonths, 'jul', "It's summer in july...");
@@ -134,21 +134,21 @@ suite('Unit Tests', function(){
     return '# name: ' + name + ', age: ' + age + '\n';
   };
   suite('Strings', function(){
-    
+
     /** 13 - #isString asserts that the actual value is a string. **/
     test('#isString, #isNotString', function() {
       assert.fail(Math.sin(Math.PI/4), 'a float is not a string');
       assert.fail(process.env.NODE_ENGINE, 'env vars are strings (or undefined)');
       assert.fail(JSON.stringify({type: 'object'}), 'a JSON is a string');
     });
-    
+
     /** 14 - #include (on #notInclude ) works for strings too !! **/
     // It asserts that the actual string contains the expected substring
     test('String #include, #notInclude', function() {
       assert.fail('Arrow', 'row', "Arrow contains row...");
       assert.fail('dart', 'queue', "But a dart doesn't contain a queue");
     });
-    
+
     /** 15 - #match Asserts that th actual value **/
     // matches the second argument regular expression.
     test('#match, #notMatch', function() {
@@ -157,8 +157,8 @@ suite('Unit Tests', function(){
       assert.fail(formatPeople('Paul Smith III', 'twenty-four'), regex);
     });
   });
-  
- // ----------------------------------------------------------------------------- 
+
+ // -----------------------------------------------------------------------------
 
   // These variables are used in the tests. Don't Edit them.
   var Car = function() {
@@ -173,12 +173,12 @@ suite('Unit Tests', function(){
     this.wheels = 6;
     this.wings = 2;
   };
-  
+
   var myCar = new Car();
   var airlinePlane = new Plane();
 
   suite('Objects', function(){
-    
+
     /** 16 - #property asserts that the actual object has a given property. **/
     // Use #property or #notProperty where appropriate
     test('#property, #notProperty', function() {
@@ -188,7 +188,7 @@ suite('Unit Tests', function(){
     });
 
     test('#typeof, #notTypeOf', function() {
-      
+
       /** 17 #typeOf asserts that value’s type is the given string, **/
       // as determined by Object.prototype.toString.
       // Use #typeOf or #notTypeOf where appropriate
@@ -200,7 +200,7 @@ suite('Unit Tests', function(){
     });
 
     test('#instanceOf, #notInstanceOf', function() {
-      
+
       /** 18 #instanceOf asserts that an object is an instance of a constructor **/
       // Use #instanceOf or #notInstanceOf where appropriate
       assert.fail(myCar, Plane);
@@ -209,11 +209,11 @@ suite('Unit Tests', function(){
       assert.fail(myCar.wheels, String );
     });
   });
-  
+
 // -----------------------------------------------------------------------------
 });
 
-/** 
- * Good Job, You are done here !!! 
- *  Please go to the file "2_functional_tests.js" ... 
+/**
+ * Good Job, You are done here !!!
+ *  Please go to the file "2_functional_tests.js" ...
  **/
